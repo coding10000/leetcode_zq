0788e84fda3764aac118da8ff7a0fbba11f013f8
张琪 <coding10000@users.noreply.github.com>
2015-06-08 17:42:47 +0800

    Update PalindromePartitionII.cpp

--- PalindromePartitionII.cpp
+++ PalindromePartitionII.cpp
@@ -12,7 +12,51 @@ Return 1 since the palindrome partitioning ["aa","b"] could be produced using 1 
 /*
 Solution:
 typical dynamic programming
+function : cuts[i] = min{cuts[j] + str(j + 1...i)}
+start state : cuts[0] = 1;
+return state : return cuts[n]
 */
 
+// time : O(n^2)
+// space : O(n^2)
 int minCut(string s) {
+    int n = s.size();
+    vector<int> cuts(n + 1, 0);
+    cuts[0] = -1;
+    vector<vector<bool> > canCut(n, vector<bool>(n, false));
+    for (int i = 1; i <= n; ++i) {
+        cuts[i] = i - 1;
+        for (int j = i; j >= 1; --j) {
+            // j...i
+            canCut[j - 1][i - 1] = (s[i - 1] == s[j - 1]) && (i - j < 2 || canCut[j][i - 2]);
+            if (canCut[j - 1][i - 1]) {
+                cuts[i] = min(cuts[i], cuts[j - 1] + 1);
+            }
+        }
+    }
+    return cuts[n];
 }
+/**
+ * bug:
+ * init state : cuts[0] = -1, cuts[i] = i - 1. because the string of n length, its max cuts is n - 1
+ * eg. a ==> 0; ab ==> 1
+ */ 
+
+// reduce extra space: O(n)
+// ???
+    int minCut_2(string s) {
+        int n = s.size();
+        vector<int> cuts(n + 1);
+        vector<bool> canCut(n, true);
+        cuts[n] = -1;
+        for (int i = n - 1; i >= 0; --i) {
+            cuts[i] = cuts[i + 1] + 1;
+            for (int j = n - 1; j >= i; --j) {
+                canCut[j] = (s[i] == s[j]) && (j - i < 2 || canCut[j - 1]);
+                if (canCut[j]) {
+                    cuts[i] = min(cuts[i], cuts[j + 1] + 1);
+                }
+            }
+        }
+        return cuts[0];
+    } 
