da07c7a9362ff4c360832cf03cbb12820868c0bd
张琪 <coding10000@users.noreply.github.com>
2015-06-08 16:28:34 +0800

    Create PalindromePartitionII.cpp

---
+++ PalindromePartitionII.cpp
@@ -0,0 +1,18 @@
+/*
+Question: Palindrome Partitioning II 
+Total Accepted: 31980 Total Submissions: 163570
+Given a string s, partition s such that every substring of the partition is a palindrome.
+
+Return the minimum cuts needed for a palindrome partitioning of s.
+
+For example, given s = "aab",
+Return 1 since the palindrome partitioning ["aa","b"] could be produced using 1 cut.
+*/
+
+/*
+Solution:
+typical dynamic programming
+*/
+
+int minCut(string s) {
+}
