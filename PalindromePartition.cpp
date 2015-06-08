158a8fbd57cea441af6ecc8cd7065d9327a7d307
张琪 <coding10000@users.noreply.github.com>
2015-06-08 14:25:24 +0800

    Create PalindromePartition.cpp

---
+++ PalindromePartition.cpp
@@ -0,0 +1,22 @@
+/*
+Question: Palindrome Partitioning 
+Total Accepted: 39958 Total Submissions: 150281 
+Given a string s, partition s such that every substring of the partition is a palindrome.
+
+Return all possible palindrome partitioning of s.
+
+For example, given s = "aab",
+Return
+  [
+    ["aa","b"],
+    ["a","a","b"]
+  ]
+*/
+
+/**
+in : string, empty?
+out :
+range : 
+space :
+other: 
+*/
