ba9bee88e0adc36985b0dddc012ab2dbec7113a6
张琪 <coding10000@users.noreply.github.com>
2015-06-08 21:01:00 +0800

    Create ShortestPalindrome.cpp

---
+++ ShortestPalindrome.cpp
@@ -0,0 +1,15 @@
+/*
+Quesition: Shortest Palindrome 
+Total Accepted: 1985 Total Submissions: 12378
+Given a string S, you are allowed to convert it to a palindrome by adding characters in front of it. 
+Find and return the shortest palindrome you can find by performing this transformation.
+
+For example:
+
+Given "aacecaaa", return "aaacecaaa".
+
+Given "abcd", return "dcbabcd".
+*/
+
+string shortestPalindrome(string s) {
+}
