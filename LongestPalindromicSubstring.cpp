464a9e57fde44bf3151529e878dd9df3fd29d243
张琪 <coding10000@users.noreply.github.com>
2015-06-08 17:50:48 +0800

    Create LongestPalindromicSubstring.cpp

---
+++ LongestPalindromicSubstring.cpp
@@ -0,0 +1,15 @@
+/*
+Question: Longest Palindromic Substring 
+Total Accepted: 52444 Total Submissions: 252838
+Given a string S, find the longest palindromic substring in S. 
+You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
+*/
+
+/*
+Soluton:
+    dp, the method is same as palindrome partition II
+*/
+
+string longestPalindrome(string s) {
+    
+}
