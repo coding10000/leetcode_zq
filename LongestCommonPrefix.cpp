56b73eb6f50a14c5f421f72fb6815229f168654f
张琪 <coding10000@users.noreply.github.com>
2015-06-09 21:53:36 +0800

    Update LongestCommonPrefix.cpp

--- LongestCommonPrefix.cpp
+++ LongestCommonPrefix.cpp
@@ -5,10 +5,46 @@ Write a function to find the longest common prefix string amongst an array of st
 */
 
 /*
-Solution:
-
+Solution: (21:36-)
+    1. use a string to compare with others, then find a min common substring
+    2. more concise
 */
 
+// more concise code
 string longestCommonPrefix(vector<string>& strs) {
-    
+    if (strs.empty() || strs[0].empty()) {
+        return string();
+    }
+    for (int i = 0; i < strs[0].size(); ++i) {
+        for (int j = 1; j < strs.size(); ++j) {
+            if (i >= strs[j].size() || strs[j][i] != strs[0][i]) {
+                return strs[0].substr(0, i);       
+            }
+        }
+    }
+    return strs[0];
 }
+
+string longestCommonPrefix(vector<string>& strs) {
+    if (strs.empty() || strs[0].empty()) {
+        return string();
+    }
+    int end = 0;
+    for (int i = 0; i < strs[0].size(); ++i) {
+        bool isExit = false;
+        for (int j = 1; j < strs.size(); ++j) {
+            if (i >= strs[j].size() || strs[j][i] != strs[0][i]) {
+                isExit = true;
+                break;
+            }
+        }
+        if (isExit) break;
+        end = i;
+    }
+    return strs[0].substr(0, end + 1);
+}
+
+/**
+ * bug:
+ * line 15: init, end = -1, not end = 0(表示默认第一个字符就是公共前缀);--> 一定要仔细！！！
+ */ 
