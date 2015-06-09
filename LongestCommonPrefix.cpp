10a042025b3465e9045ec935184d05ae125613fc
张琪 <coding10000@users.noreply.github.com>
2015-06-09 21:35:04 +0800

    Create LongestCommonPrefix.cpp

---
+++ LongestCommonPrefix.cpp
@@ -0,0 +1,14 @@
+/*
+Longest Common Prefix 
+Total Accepted: 50128 Total Submissions: 194829
+Write a function to find the longest common prefix string amongst an array of strings.
+*/
+
+/*
+Solution:
+
+*/
+
+string longestCommonPrefix(vector<string>& strs) {
+    
+}
