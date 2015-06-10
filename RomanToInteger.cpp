6f511ac964a39d4ff87e2d89ccf29375a1ebf157
张琪 <coding10000@users.noreply.github.com>
2015-06-10 23:42:28 +0800

    Update RomanToInteger.cpp

--- RomanToInteger.cpp
+++ RomanToInteger.cpp
@@ -10,5 +10,14 @@ Solution:
 */
 
 int romanToInt(string s) {
-
+    unordered_map<char, int> dict({{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}});
+    int res = 0;
+    for (int i = 0; i < s.size(); ++i) {
+        if (i < s.size() - 1 && dict[s[i]] < dict[s[i + 1]]) {
+            res -= dict[s[i]];
+        } else {
+            res += dict[s[i]];
+        }
+    }
+    return res;
 }
