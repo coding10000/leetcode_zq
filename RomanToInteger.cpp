cfe92411b92915dd3f9ef2da2637adaa1b3e7a6a
张琪 <coding10000@users.noreply.github.com>
2015-06-10 22:26:24 +0800

    Create RomanToInteger.cpp

---
+++ RomanToInteger.cpp
@@ -0,0 +1,16 @@
+/*
+Roman to Integer Total Accepted: 39968 Total Submissions: 113569
+Given a roman numeral, convert it to an integer.
+Input is guaranteed to be within the range from 1 to 3999.
+*/
+
+/*
+题意理解：
+1、罗马数字共有7个，即I（1）、V（5）、X（10）、L（50）、C（100）、D（500）和M（1000）。
+    eg. I, II, III, IV, V, VI, VII, VIII, IX, X, XI,....,XX,..XXX...XL...LX...LXX..
+2、左减右加逻辑，以及右加不能超过三位，左减只能有1位。
+*/
+
+int romanToInt(string s) {
+
+}
