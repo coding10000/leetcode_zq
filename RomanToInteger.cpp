4e97f77ad3d14c808cc462cba20e594d35ba638a
张琪 <coding10000@users.noreply.github.com>
2015-06-10 23:29:43 +0800

    Create RomanToInteger.cpp

---
+++ RomanToInteger.cpp
@@ -0,0 +1,14 @@
+/*
+Roman to Integer
+Given a roman numeral, convert it to an integer.
+Input is guaranteed to be within the range from 1 to 3999.
+*/
+
+/*
+Solution:
+1. hash table + string
+*/
+
+int romanToInt(string s) {
+
+}
