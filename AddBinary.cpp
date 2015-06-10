e81c5b5c08db595aa258f96465124b9e7821f9cf
张琪 <coding10000@users.noreply.github.com>
2015-06-11 00:21:43 +0800

    Update AddBinary.cpp

--- AddBinary.cpp
+++ AddBinary.cpp
@@ -30,39 +30,12 @@ string addBinary(string a, string b) {
         }
         // bug 1:
         // res = '0' + r % 2 + res;
-        // 转换成字符之后才能加操作
+         // 转换成字符之后才能加操作
         res = static_cast<char>('0' + r % 2) + res;
         r = r / 2;
     }
-    res = static_cast<char>('0' + r % 2) + res;
-    // bug2:
-    // 最后一步去掉前面的所有0: 以防"0000"的前面全0情况
-    int start = 0;
-    while (res[start] == '0' && start < res.size() - 1) ++start;
-    return res.substr(start);
+    if (r % 2) res = static_cast<char>('0' + r % 2) + res;
+    return res;
 }
-
-// more concise
-  string addBinary(string a, string b) {
-      int i = a.size() - 1;
-      int j = b.size() - 1;
-      string res;
-      int r = 0;
-      while (i >= 0 || j >= 0) {
-          if (i >= 0) {
-              r += a[i] - '0';
-              --i;
-          } 
-          if (j >= 0) {
-              r += b[j] - '0';
-              --j;
-          }
-          res = static_cast<char>('0' + r % 2) + res;
-          r = r / 2;
-      }
-      if (r % 2) res = static_cast<char>('0' + r % 2) + res;
-      if (res[0] == '0') return "0";
-      return res;
-  }
 
 
