ef96dff7005bed1fee22cffb287144b3339bf6cc
张琪 <coding10000@users.noreply.github.com>
2015-06-10 23:45:29 +0800

    Create AddBinary.cpp

---
+++ AddBinary.cpp
@@ -0,0 +1,19 @@
+/*
+Add Binary
+Given two binary strings, return their sum (also a binary string).
+
+For example,
+a = "11"
+b = "1"
+Return "100".
+*/
+
+/*
+Solution:
+  from end to start
+  2 into 1
+*/
+
+string addBinary(string a, string b) {
+
+}
