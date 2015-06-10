365e44f3e8dfcf948cc3bd5e25b5d4659eb38072
张琪 <coding10000@users.noreply.github.com>
2015-06-10 23:24:04 +0800

    Update RomanToInteger.cpp

--- RomanToInteger.cpp
+++ RomanToInteger.cpp
@@ -1,6 +1,7 @@
 /*
-Roman to Integer Total Accepted: 39968 Total Submissions: 113569
-Given a roman numeral, convert it to an integer.
+Integer to Roman
+Given an integer, convert it to a roman numeral.
+
 Input is guaranteed to be within the range from 1 to 3999.
 */
 
@@ -11,6 +12,33 @@ Input is guaranteed to be within the range from 1 to 3999.
 2、左减右加逻辑，以及右加不能超过三位，左减只能有1位。
 */
 
-int romanToInt(string s) {
+/**
+ * Solution:
+ * two vector(hash) + string
+ */ 
 
+string intToRoman(int num) {
+    if (num <= 0) return string();
+    vector<int> integer({1,4,5,9,10,40,50,90,100,400,500,900,1000});
+    vector<string> roman({"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"});
+    string res = "";
+    // bug : from end(big) to start(small)!!!!
+    //for (int i = 0; i < integer.size() && num; ++i) {
+    for (int i = integer.size() - 1; i >= 0 && num; --i) {
+        int r = num / integer[i];
+        if (r) {
+            while (r--) res += roman[i];
+            num = num % integer[i];
+        }
+    }
+    /* // more concise
+    for (int i = integer.size() - 1; i >= 0; --i) {
+        while (integer[i] <= num) {
+            res += roman[i];
+            num -= integer[i];
+        }
+    }
+    return res;
+    */
+    return res;
 }
