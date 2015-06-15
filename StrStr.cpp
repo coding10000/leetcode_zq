c2d029dfbc73182214ed737f70405666ea6b19a7
张琪 <coding10000@users.noreply.github.com>
2015-06-15 15:20:14 +0800

    Update StrStr.cpp

--- StrStr.cpp
+++ StrStr.cpp
@@ -30,6 +30,51 @@ please click the reload button  to reset your code definition.
     return -1;
  }
  
- int strStrKMP(string haystack, string needle) {
-  
- }
+    void getNext(const string& s, vector<int>& next) {
+        next[1] = 0;
+        int k = 0; // prefix length
+        for (int i = 2; i <= s.size(); ++i) {
+            // opotimize
+            //if (s[i - 1] == s[next[i - 1]]) next[i - 1] = next[next[i - 1]];
+            // compare current char(即 max suffix's next char) and max prefix's next char
+                // (1) not equal, recursively find the previous max prefix
+            while (k > 0 && s[i - 1] != s[k]) {
+                k = next[k];
+            }
+                // (2) equal, ++k(that is, prefix's length++) 
+            if (s[i - 1] == s[k]) {
+                ++k;
+            }
+            // next array i-th position's prefix length is k
+            next[i] = k;
+        }
+    }
+
+    int strStrKMP(string haystack, string needle) {
+        if (needle.empty()) return 0;
+        if (haystack.size() < needle.size()) return -1;
+        int n1 = haystack.size();
+        int n2 = needle.size();
+        vector<int> next(n2 + 1, 0);
+        getNext(needle, next);
+        int i = 0; // current string position
+        int j = 0; // mathced length
+        while (i < n1) {
+            // match, ++i, ++j
+            while (j < n2 && haystack[i] == needle[j]) {
+                ++i;
+                ++j;
+            }
+            // j == 0 represents no common prefix, move to next char(++i)
+            if (j == 0) {
+                ++i;
+            }
+            // j == n2 reprensents find a mathed position, return
+            if (j == n2) {
+                return i - j;
+            }
+            // j != n2 && j != 0, jump to the max prefix
+            j = next[j];
+        }
+        return -1;
+    }
